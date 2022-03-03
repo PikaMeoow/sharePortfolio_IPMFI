661451d0b7ce3ec6b259b98b5f75eceea5da55aa
Arielle <zhaomengying_fr@outlook.com>
2022-03-03 20:56:49 +0100

    Second Commit

---
+++ src/test/java/tp10/metier/JourTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package tp10.metier;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author zhaom
+ */
+public class JourTest {
+    
+    public JourTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
