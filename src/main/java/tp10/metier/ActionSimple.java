04608ad6854d98b364924edea7d082bf3ed61a06
Arielle <zhaomengying_fr@outlook.com>
2022-03-03 21:57:26 +0100

    Fifth Commit

--- src/main/java/tp10/metier/ActionSimple.java
+++ src/main/java/tp10/metier/ActionSimple.java
@@ -1,14 +1,8 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,46 +20,47 @@ import java.util.HashMap;
 
 /**
  *
- * les actions simples sont des actions !
- *   heritage --> donc (i) concretiser la methode abstraire 'valeur'
- *                  et (ii) def d'un constructeur cohérent avec le constructeur 
- *                                   parent
+ * les actions simples sont des actions ! heritage --> donc (i) concretiser la
+ * methode abstraire 'valeur' et (ii) def d'un constructeur cohérent avec le
+ * constructeur parent
  */
 public class ActionSimple extends Action {
-    
+
     // Table de hachage contenant les cours indexés par des jours
     private HashMap<Jour, Cours> tblCours;
-    
+
     // Constructeur cohérent avec le constructeur parent
     public ActionSimple(String nom) {
         super(nom); // c'est d'abord 1 action !
         this.tblCours = new HashMap<Jour, Cours>();
     }
-    
+
     public void enrgCours(Jour j, float px) {
         /* création d'un objet Cours associé au prix
          *    puis ajout dans la table en indexant par le jour
-         */        
-        if(this.tblCours.containsKey(j) == true)
+         */
+        if (this.tblCours.containsKey(j) == true) {
             this.tblCours.remove(j);
-        
+        }
+
         this.tblCours.put(j, new Cours(px));
-            
-        
+
     }
-    
+
     @Override
     public float valeur(Jour j) {
         // A FAIRE !
-        if(this.tblCours.get(j) != null)
+        if (this.tblCours.get(j) != null) {
             return this.tblCours.get(j).getValeur();
-        else
+        } else {
             return 0; // cours nul si pas de pas de cours
+        }
     }
-   /* classee privée !! 
+
+    /* classee privée !! 
     *   la classe n'est accessible que depuis la classe englobante
-    */
-    
+     */
+
     private class Cours {
 
         private float valeur;
@@ -81,8 +76,7 @@ public class ActionSimple extends Action {
 
         public Cours(float valeur) {
             this.valeur = valeur;
-        }        
+        }
     }
-    
-    
+
 }
