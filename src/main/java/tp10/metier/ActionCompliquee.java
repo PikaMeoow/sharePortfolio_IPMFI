9993d73a68ca50449aef66afe0fd490ab8045cf6
Arielle <zhaomengying_fr@outlook.com>
2022-03-03 21:19:24 +0100

    Third Commit

--- src/main/java/tp10/metier/ActionCompliquee.java
+++ src/main/java/tp10/metier/ActionCompliquee.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -19,32 +37,33 @@ public class ActionCompliquee extends Action {
         super(nom);
         this.tblComposition = new HashMap<ActionSimple, Proportion>();
     }
-    
-    
+
     @Override
     public float valeur(Jour j) {
         float total;
-        
+
         total = 0;
-        
-         // parcours des clefs
-        for(ActionSimple act : this.tblComposition.keySet()) {
-            total = total + 
-                    (act.valeur(j) * this.tblComposition.get(act).getPourcentage());
+
+        // parcours des clefs
+        for (ActionSimple act : this.tblComposition.keySet()) {
+            total = total
+                    + (act.valeur(j) * this.tblComposition.get(act).getPourcentage());
         }
         return total;
     }
-    
+
     public void enrgProportion(ActionSimple as, float pourcentage) {
-        if(this.tblComposition.containsKey(as) == true)
+        if (this.tblComposition.containsKey(as) == true) {
             this.tblComposition.remove(as);
-           
+        }
+
         this.tblComposition.put(as, new Proportion(pourcentage));
     }
-    
+
     public String toString() {
-        return this.getNom() +"=>" + this.tblComposition.toString(); 
+        return this.getNom() + "=>" + this.tblComposition.toString();
     }
+
     private class Proportion {
 
         private float pourcentage;
--- src/main/java/tp10/metier/Portefeuille.java
+++ src/main/java/tp10/metier/Portefeuille.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -11,71 +29,75 @@ import java.util.HashMap;
  * @author laurent
  */
 public class Portefeuille {
-    
+
     private HashMap<Action, LignePortefeuille> lignes;
-    
+
     public Portefeuille() {
         this.lignes = new HashMap<Action, LignePortefeuille>();
     }
-    
-    public void acheter(Action action, int qte) {        
-        if(this.lignes.get(action) == null)
+
+    public void acheter(Action action, int qte) {
+        if (this.lignes.get(action) == null) {
             this.lignes.put(action, new LignePortefeuille(qte));
-        else
-           this.lignes.get(action).augmenter(qte); 
-    }
-    
-    
-    public void vendre(Action action, int qte) {        
-        if(this.lignes.get(action) != null) { // si la ligne existe 
-           this.lignes.get(action).diminuer(qte);  // la qte est modifiée
-           
-            if(this.lignes.get(action).getQte() == 0) // reste-il encore
-                this.lignes.remove(action);  // des actions dans la ligne
+        } else {
+            this.lignes.get(action).augmenter(qte);
         }
     }
+
+    public void vendre(Action action, int qte) {
+        if (this.lignes.get(action) != null) { // si la ligne existe 
+            this.lignes.get(action).diminuer(qte);  // la qte est modifiée
+
+            if (this.lignes.get(action).getQte() == 0) // reste-il encore
+            {
+                this.lignes.remove(action);  // des actions dans la ligne
+            }
+        }
+    }
+
     /* A améliorer car on suppose que chaque action est valorisable le jour j  
      *     calcul à bloquer si une action n'est pas quotée par exemple.
      */
     public float valeur(Jour j) {
         float valeurTotale;
-        
+
         valeurTotale = 0;
-        for(Action act : this.lignes.keySet()) {
-            valeurTotale = valeurTotale + 
-                    (this.lignes.get(act).getQte() * act.valeur(j));
-        }        
+        for (Action act : this.lignes.keySet()) {
+            valeurTotale = valeurTotale
+                    + (this.lignes.get(act).getQte() * act.valeur(j));
+        }
         return valeurTotale;
     }
-    
-    
+
     @Override
-    public String toString() {        
+    public String toString() {
         return this.lignes.toString();
     }
-    
+
     private class LignePortefeuille {
+
         private int qte;
 
         public int getQte() {
             return qte;
         }
-        
+
         public void augmenter(int qteAAjouter) {
-            if(qteAAjouter > 0)
+            if (qteAAjouter > 0) {
                 this.qte = this.qte + qteAAjouter;
+            }
         }
-        
+
         public void diminuer(int qteASoustraire) {
-            if(qteASoustraire <= this.qte && qteASoustraire > 0) {
+            if (qteASoustraire <= this.qte && qteASoustraire > 0) {
                 qte = qte - qteASoustraire;
             }
         }
 
         public LignePortefeuille(int qte) {
             this.qte = qte;
-        }   
-        
+        }
+
         @Override
         public String toString() {
             return Integer.toString(qte);
